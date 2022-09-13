76fd9a5cc949667da8de513ec3ff82411e0d5e23
omrirp <omrirp@gmail.com>
2022-09-13 13:34:43 +0300

    Add weather informatin

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,15 +3,28 @@
 const request = require('request');
 
 const weatherstack = (lat, lon, callback) => {
+  console.log(lat + ',' + lon);
   let url = `http://api.weatherstack.com/current?access_key=ba53b263d6b90ec3ebbdf4c911076537&query=${lat},${lon}`;
   //({ url, json: true }, (error, response.body)
   request({ url, json: true }, (error, { body }) => {
+    //console.log(body);
     if (error) {
       callback('Unable to connect to weather services!', undefined);
     } else if (body.error) {
       callback('Unable to find location!', undefined);
     } else {
-      callback(undefined, 'Temperature: ' + body.current.temperature + '\nFeels like: ' + body.current.feelslike);
+      console.log(body);
+      callback(
+        undefined,
+        'Temperature: ' +
+          body.current.temperature +
+          '\nFeels like: ' +
+          body.current.feelslike +
+          '\nUV index: ' +
+          body.current.uv_index +
+          '\nHumidity: ' +
+          body.current.humidity
+      );
     }
   });
 };
